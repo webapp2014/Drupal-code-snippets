700bea8d705fcc29a29f8ce070df04f11b620b5d
David <drupalngo@gmail.com>
2018-05-02 14:52:00 +0800

    new code

---
+++ theme.php
@@ -0,0 +1,6 @@
+// theme 
+Out Now: Glazed Builder & Glazed Theme For Drupal 8! Check Out Our Revolutionary New Page Building Experience For Drupal
+
+https://www.sooperthemes.com/drupal-blog/out-now-glazed-builder-glazed-theme-drupal-8-check-out-our-revolutionary-new-page
+
+https://www.drupal.org/project/stacks
